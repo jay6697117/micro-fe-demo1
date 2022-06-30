c557e81c756419bbdfb35d30e8bd1822ea956e8e
steve zhang <871506263@qq.com>
2022-06-30 20:43:40 +0800

    提交代码于: 2022-06-30 20:43:40

--- sub-react/src/App.js
+++ sub-react/src/App.js
@@ -1,17 +1,15 @@
 import logo from './assets/logo.svg';
 import './assets/App.css';
-// import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <div className='Browser-router'>
-          {/* <BrowserRouter>
-            <Link to='/vue'>vue子应用</Link>
-            &nbsp;&nbsp;&nbsp;
-            <Link to='/'>主页</Link>
-          </BrowserRouter> */}
+          <BrowserRouter>
+            <Link to='/'>基座主页</Link>
+          </BrowserRouter>
         </div>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
