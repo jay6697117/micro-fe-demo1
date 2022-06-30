5e80bc7d8cf5496ed4f144bfbd774c7672a0f2e5
steve zhang <871506263@qq.com>
2022-06-30 20:30:51 +0800

    提交代码于: 2022-06-30 20:30:51

--- src/registerApps.js
+++ src/registerApps.js
@@ -9,14 +9,14 @@ const loader = loading => {
 registerMicroApps(
   [
     {
-      name: 'son-vue', // 子应用的名称
+      name: 'sub-vue', // 子应用的名称
       entry: 'http://localhost:8080', // 子应用的访问地址
       container: '#container', // 子应用应该挂载的位置（后面会解释）
       activeRule: '/vue', // 在路径为xxx的时候让子应用渲染
       loader // loader是乾坤提供的一个类似于加载中的函数
     },
     {
-      name: 'son-react',
+      name: 'sub-react',
       entry: 'http://localhost:3001',
       container: '#container',
       activeRule: '/react',
